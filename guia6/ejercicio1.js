d7c381b4c94364ecd38c87b1e15459618f930c79
Matias Marchese <matiasmarchese23@gmail.com>
2024-02-25 13:55:19 -0300

    Punto a ejercicio 1 guia 6

--- guia6/ejercicio1.js
+++ guia6/ejercicio1.js
@@ -1,4 +1,4 @@
-// Se dispone de una lista de 10 grupos de números enteros separados entre ellos
+// Se dispone de una lista de 10 grupos de números enteros separados entre ellos 
 // por ceros. Se pide determinar e informar:
 // a. El número de grupo con mayor porcentaje de números impares
 // positivos respecto al total de números que forman el grupo.
@@ -8,3 +8,29 @@
 // c. Informar cuántos grupos están formados por todos números ordenados
 // de mayor a menor.
 
+let grupo = 0;
+let porcentajeImpares = 0;
+
+for(let i = 0; i < 5; i++) {
+    let n = parseInt(prompt("Ingresar numero"));
+    let totalGrupo = 0;
+    let impares = 0;
+    let porcentaje;
+
+    while(n !== 0) {
+        totalGrupo++;
+
+        if(n % 2 !== 0){
+            impares++;
+        }
+
+        n = parseInt(prompt("Ingresar numero"));
+    }
+
+    porcentaje = (impares * 100) / totalGrupo;
+
+    if(porcentaje > porcentajeImpares) {
+        porcentajeImpares = porcentaje;
+        grupo = i;
+    }
+}
